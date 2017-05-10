3b6ab63ecd0379f32943922b2432680d537c393d
minhlinh <minhlinh100@gmail.com>
2017-05-10 15:04:17 +0700

    Create all_m.js

---
+++ all_m.js
@@ -0,0 +1,14 @@
+document.writeln("<script language=\"javascript\" src=\"http://vnlive.dakhoaquocte.com.vn/JS/LsJS.aspx?siteid=KQG86764836&lng=en\"></script>");
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+}
+else
+{
+    document.writeln("<style>#LRfloater1{display:none}</style>");
+}
+document.writeln("<style>#LRfloater2{max-height: 100%;}</style>");
+if(!window.jQuery) {
+document.writeln("<script language=\"javascript\" src=\"http://chuyende.phongkhamngoquyen.com/js_all/jquery-1.11.3.min.js\"></script>");
+}
+document.writeln("<script language=\"javascript\" src=\"http://chuyende.phongkhamngoquyen.com/js_all/all_form.js\"></script>");
+
+document.writeln("<script language=\"javascript\" src=\"http://chuyende.phongkhamngoquyen.com/js_all/all_formcall.js\"></script>");
